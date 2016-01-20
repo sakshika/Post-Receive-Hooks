33e204ebede8a8adf0a5f03ddec0ff2ce29bb48e
sakshi <sakshik@cybage.com>
2016-01-20 18:05:37 +0530

    first commit

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+ls
+cd /opt
+ls
+which composer 
+composer config http-basic.example.org abhijeetka abhijeet123#
+composer config -g github-oauth.github.com 0a3258c80976f2dfd65ba75f09e97d58937515a3
+#composer update
+#composer install
+ant -version
+ant
+ls
